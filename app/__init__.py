3776491f12331b9bd375a03aeb2d7496e2186c8a
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-06-20 17:09:57 -0700

    Previous Work Experience added usin gthe Jinja template

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    work_experience = [
+        {
+            "company": "Nintendo",
+            "position": "Customer Support",
+            "description": "Assisted customers with technical issues."
+        },
+        {
+            "company": "Best Buy",
+            "position": "Inventory Specialist",
+            "description": "Compared stock to online inventory to ensure acccuracy."
+        },
+        {
+            "company": "Green River College",
+            "position": "Full Stack Developer Intern",
+            "description": "Worked with professors to update tools and resources they used to manage their classes."
+        }
+    ]
+    
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), work_experience=work_experience)
