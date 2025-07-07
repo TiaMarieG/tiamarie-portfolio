fb006c4507df24831154d9b2646e550e41fe1e1c
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-07-07 11:34:11 -0700

    Fixed an error with incorrect spacing and created a database table. Also created post and get routes to that will save information to the table and display it

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 import os
+import datetime
+
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from peewee import *
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
 
-mydb = 
-MySQLDatabase(
+mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
@@ -16,6 +18,18 @@ MySQLDatabase(
 )
 
 print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default = datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 NAV_LINKS = [
     {"name": "Home", "endpoint": "index"},
@@ -76,3 +90,21 @@ from datetime import datetime
 @app.context_processor
 def inject_globals():
     return dict(nav_links=NAV_LINKS, now=datetime.now)
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name=request.form['name'],
+    email=request.form['email'],
+    content=request.form['content'],
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
